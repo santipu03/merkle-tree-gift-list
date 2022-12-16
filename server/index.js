5bae8636a049508e8a785e55a199067b8720d7d8
santipu <santipujolsau@gmail.com>
2022-12-16 16:49:21 +0100

    implemented send and verification of a name in the Merkle Tree

--- server/index.js
+++ server/index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "9cd44a2629d3fb73d1418216fc33a18509eda38271d77713a4dfdb8b93e3e1a0";
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
+app.post("/gift", (req, res) => {
   const body = req.body;
+  const proof = body.proof;
+  const name = body.name;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
